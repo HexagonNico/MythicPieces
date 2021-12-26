388ee5df852ee41146543a2b1ba20f56e04a1c58
Nico <nico.hex6@gmail.com>
2021-12-26 16:25:36 +0100

    Collision between map pieces

--- scripts/map_piece.gd
+++ scripts/map_piece.gd
@@ -1,19 +1,76 @@
 extends Node2D
 
+
+enum Status {
+	DEFAULT,
+	SELECTED,
+	DRAGGING,
+	ERROR,
+}
+
 # Inspector variables
-export var red_tint: float = 0.15
-export var green_tint: float = 0.15
-export var blue_tint: float = 0.15
+export var selection_color: Color = Color(0.15, 0.15, 0.15, 1)
+export var error_color: Color = Color(0.2, 0.0, 0.0, 1)
+
+# Varying
+var status = Status.DEFAULT
+var drag_delta: Vector2 = Vector2(0, 0)
+var position_before: Vector2 = Vector2(0, 0)
 
 
-# Receives signal from Area2D
-func _on_mouse_entered():
-	var color_tint: Color = Color(self.red_tint, self.green_tint, self.blue_tint, 0.0)
+# Called every frame
+func _process(_delta: float):
+	# Dragging
+	if self.status == Status.DRAGGING or self.status == Status.ERROR:
+		var mouse_pos: Vector2 = self.get_viewport().get_mouse_position()
+		var unmodulated: Vector2 = mouse_pos - self.drag_delta
+		self.position = Vector2(round(unmodulated.x / 32) * 32, round(unmodulated.y / 32) * 32)
+	
+	# Color tint
+	var color_tint: Color = Color(0.0, 0.0, 0.0, 0.0)
+	match self.status:
+		Status.SELECTED, Status.DRAGGING:
+			color_tint = Color(self.selection_color.r, self.selection_color.g, self.selection_color.b, 0.0)
+		Status.ERROR:
+			color_tint = Color(self.error_color.r, self.error_color.g, self.error_color.b, 0.0)
 	self.material.set_shader_param("color_additive", color_tint)
-	print("Mouse entered")
 
 
-# Receives signal from Area2D
-func _on_mouse_exited():
-	self.material.set_shader_param("color_additive", Color(0.0, 0.0, 0.0, 0.0))
-	print("Mouse exited")
+# Called at every input event
+func _input(event: InputEvent):
+	if event.is_action("select_map_piece"):
+		if event.is_pressed():
+			if self.status == Status.SELECTED:
+				self.status = Status.DRAGGING
+				self.drag_delta = self.get_viewport().get_mouse_position() - self.position
+				self.position_before = self.position
+		else:
+			if self.status == Status.ERROR:
+				self.position = self.position_before
+				self.status = Status.DEFAULT
+			elif self.status == Status.DRAGGING:
+				self.status = Status.SELECTED
+
+
+# Receives mouse_entered signal from SelectionArea
+func _on_SelectionArea_mouse_entered():
+	if self.status == Status.DEFAULT:
+		self.status = Status.SELECTED
+
+
+# Receives mouse_exited signal from SelectionArea
+func _on_SelectionArea_mouse_exited():
+	if self.status == Status.SELECTED:
+		self.status = Status.DEFAULT
+
+
+# Receives area_entered signal from CollisionArea
+func _on_CollisionArea_area_entered(_area: Area2D):
+	if self.status == Status.DRAGGING:
+		self.status = Status.ERROR
+
+
+# Receives area_exited signal from CollisionArea
+func _on_CollisionArea_area_exited(_area: Area2D):
+	if self.status == Status.ERROR:
+		self.status = Status.DRAGGING
